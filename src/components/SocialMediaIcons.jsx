eadd3de5acf60e434769d550153c2a3a8c496f80
prasetyamahaputra <prasetyamaha99@gmail.com>
2023-01-24 00:52:15 +0800

    create landing component and socialMediaIcons

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,40 @@
+const SocialMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        href="https://www.linkedin.com"
+        className="hover:opacity-50 transition duration-500"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="../assets/linkedin.png" alt="linkedin-link" />
+      </a>
+      <a
+        href="https://www.twitter.com"
+        className="hover:opacity-50 transition duration-500"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="../assets/twitter.png" alt="linkedin-link" />
+      </a>
+      <a
+        href="https://www.facebook.com"
+        className="hover:opacity-50 transition duration-500"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="../assets/facebook.png" alt="linkedin-link" />
+      </a>
+      <a
+        href="https://www.instagram.com"
+        className="hover:opacity-50 transition duration-500"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="../assets/instagram.png" alt="linkedin-link" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
