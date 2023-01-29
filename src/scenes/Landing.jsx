f8cd7015a29a0d234c49f000d3b03851b744f836
prasetyamahaputra <prasetyamaha99@gmail.com>
2023-01-29 23:44:11 +0800

    create my skills page

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Landing = ({ setSelectedPage }) => {
-  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   const isAboveSmall = useMediaQuery("(max-width: 480px)");
 
   return (
@@ -14,7 +14,7 @@ const Landing = ({ setSelectedPage }) => {
     >
       {/* IMAGE SECTION */}
       <div className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
-        {isAboveLarge ? (
+        {isAboveMediumScreens ? (
           <div
             className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
             before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
@@ -22,14 +22,14 @@ const Landing = ({ setSelectedPage }) => {
             <img
               alt="profile"
               className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
-              src="assets/profile-image.jpg"
+              src="assets/pras-profile.png"
             />
           </div>
         ) : (
           <img
             alt="profile"
             className="z-10 w-full max-w-[400px] md:max-w-[600px]"
-            src="assets/profile-image.jpg"
+            src="assets/pras-profile.png"
           />
         )}
       </div>
