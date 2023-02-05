e923b5e76a8b76d6fb05ac96ca2576a5d6784f96
prasetyamahaputra <prasetyamaha99@gmail.com>
2023-02-05 23:09:34 +0800

    update

--- src/scenes/Testimonials.jsx
+++ src/scenes/Testimonials.jsx
@@ -2,5 +2,9 @@ import LineGradient from "../components/LineGradient";
 import { motion } from "framer-motion";
 
 const Testimonials = () => {
-    
+    return (
+        <div>
+            Hello from testimonials
+        </div>
+    )
 }
